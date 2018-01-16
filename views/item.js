1d36a74f89576d786976f6f29bc84e7a5cff1f33
btheado <brian.theado@gmail.com>
2018-01-15 19:31:14 -0500

    cleanup

--- views/item.js
+++ views/item.js
@@ -2,14 +2,6 @@ var html = require('choo/html')
 
 module.exports = view
 
-// item state fields:
-// children
-// sorted - not used for any display purposes?
-// checkbox prefix/dom element?, sort action: swap vs. noswap
-// id/idx?
-// expanded/collapsed/leaf and bullet click action
-// header - for displaying the question on previous line
-// title
 function view (item, emit) {
   var children = ''
   var bullet = '•'
@@ -18,6 +10,8 @@ function view (item, emit) {
       bullet = '►'
     } else {
       bullet = '▼'
+
+      // Recursively construct the descendants
       children = html`
         <div class='node-children pl3'>
           ${item.children.map(child => view(child, emit))}
